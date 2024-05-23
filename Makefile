0d285d632bd37f05683c851bbed58d63238a9529
dima020 <lozhkin-99@ya.ru>
2024-05-23 21:36:24 +0300

    4 задание

--- Makefile
+++ Makefile
@@ -8,3 +8,5 @@ publish:
 	poetry publish --dry-run
 package-install:
 	pip install --user dist/*.whl
+lint:
+	poetry run flake8 brain_games
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
-
-from brain_games.scripts.cli import welcome_user
+from brain_games.cli import welcome_user
 
 
 def greet():
-    print(f'Welcome to the Brain Games!')
+    print('Welcome to the Brain Games!')
+
 
 def main():
     greet()
     welcome_user()
+
 
 if __name__ == '__main__':
     main()
